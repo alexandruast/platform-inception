8a15a9b5711016146b8578540f5242a37533edb8
Alexandru Ast <alexandru.ast@gmail.com>
2018-02-28 12:33:54 +0200

    Replaced branches devel with master in origin/master

--- origin/jobs/scm/pipeline-jenkins-deploy.groovy
+++ origin/jobs/scm/pipeline-jenkins-deploy.groovy
@@ -10,7 +10,7 @@ node {
     }
     stage('preparation') {
       checkout([$class: 'GitSCM', 
-        branches: [[name: '*/devel']], 
+        branches: [[name: '*/master']], 
         doGenerateSubmoduleConfigurations: false, 
         submoduleCfg: [], 
         userRemoteConfigs: [[url: 'https://github.com/alexandruast/platform-inception.git']]])
