281b3305a6d4d6cabc1d53c5446293eb26f75b83
Alexandru Ast <alexandru.ast@gmail.com>
2018-03-22 07:34:39 +0200

    Work in progress

--- docker/fluentd/entrypoint.sh
+++ docker/fluentd/entrypoint.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 touch /var/log/fluentd.pos_file
+CONFIG="/fluentd/etc/fluent.conf"
+NEXT_CONFIG="/fluentd/etc/fluent.conf.next"
+config_md5_current="$(md5sum ${CONFIG})"
+# Get new config file here
+{
+  while true; do
+    if [[ -f ${NEXT_CONFIG} ]]; then
+      config_md5_next="$(md5sum ${NEXT_CONFIG})"
+      if [[ "${config_md5_current}" != "${config_md5_next}" ]]; then
+        if fluentd --dry-run -c ${NEXT_CONFIG}; then
+          cp "${NEXT_CONFIG}" "${CONFIG}"
+          config_md5_current="${config_md5_next}"
+          kill -HUP 1
+        fi
+      fi
+    fi
+    sleep 1
+  done;
+} &
 exec "$@"
