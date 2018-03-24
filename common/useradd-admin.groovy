cbae1349bd723e0d683754255304fea971cef997
Alexandru Ast <alexandru.ast@gmail.com>
2018-03-24 05:53:07 +0200

    Work in progress

--- common/useradd-admin.groovy
+++ common/useradd-admin.groovy
@@ -4,11 +4,11 @@ import hudson.security.*
 def instance = Jenkins.getInstance()
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("{{ JENKINS_ADMIN_USER }}","{{ JENKINS_ADMIN_PASS }}")
+hudsonRealm.createAccount("{{JENKINS_ADMIN_USER}}","{{JENKINS_ADMIN_PASS}}")
 instance.setSecurityRealm(hudsonRealm)
 
 def strategy = new GlobalMatrixAuthorizationStrategy()
-strategy.add(Jenkins.ADMINISTER, "{{ JENKINS_ADMIN_USER }}")
+strategy.add(Jenkins.ADMINISTER, "{{JENKINS_ADMIN_USER}}")
 instance.setAuthorizationStrategy(strategy)
 
 instance.save()
