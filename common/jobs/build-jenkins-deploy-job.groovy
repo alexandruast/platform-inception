55b520e7b0b404ab5e8bc2d179b6b2f410fd8e5b
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-07 17:44:02 +0300

    Work in progress

--- common/jobs/build-jenkins-deploy-job.groovy
+++ common/jobs/build-jenkins-deploy-job.groovy
@@ -3,7 +3,7 @@ def job = instance.getItem("{{JENKINS_BUILD_JOB}}")
 def params = [
   new PasswordParameterValue("JENKINS_ADMIN_PASS", "{{JENKINS_ADMIN_PASS}}"),
   new StringParameterValue("ANSIBLE_TARGET", "{{ANSIBLE_TARGET}}"),
-  new StringParameterValue("JENKINS_SCOPE", "{{JENKINS_SCOPE}}"),
+  new StringParameterValue("JENKINS_SCOPE", "{{JENKINS_SCOPE}}")
 ]
 def futureTask = job.scheduleBuild2(0, new ParametersAction(params))
 def build = futureTask.get()
