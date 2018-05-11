eb99396a06f7a6cae8adc96b7146a116e657211d
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-11 12:30:47 +0300

    Work in progress

--- factory/jobs/scm/pipeline-nomad-deploy.groovy
+++ factory/jobs/scm/pipeline-nomad-deploy.groovy
@@ -13,27 +13,31 @@ node {
       submoduleCfg: [], 
       userRemoteConfigs: [[url: 'https://github.com/alexandruast/platform-inception.git']]])
       sh '''
-        if [[ *"@"* == "${ANSIBLE_TARGET}" ]]; then
-          SSH_TARGET="${ANSIBLE_TARGET}"
+      declare -a SSH_TARGETS
+      for s in ${ANSIBLE_TARGET//,/ }; do
+        if [[ *"@"* == "${s}" ]]; then
+          SSH_TARGETS=("${SSH_TARGETS}" "${s}")
         else
           SSH_USER=$(echo "${ANSIBLE_EXTRAVARS}" | tr "'" '"' | jq -r .ansible_user)
           if [[ "${SSH_USER}" != "" ]]; then
-            SSH_TARGET="${SSH_USER}@${ANSIBLE_TARGET}"
+            SSH_TARGETS=("${SSH_TARGETS}" "${SSH_USER}@${s}")
           else
-            SSH_TARGET="${ANSIBLE_TARGET}"
+            SSH_TARGETS=("${SSH_TARGETS}" "${s}")
           fi
         fi
-        # echo "SSH_TARGET=${SSH_TARGET}" >> .jenkins_env
-        curl -Ss --request PUT --data "${SSH_TARGET}" http://127.0.0.1:8500/v1/kv/jenkins/pipeline_nomad_deploy_ssh_target
+      done
+      curl -Ss --request PUT --data "$(IFS=$','; echo "${SSH_TARGETS[*]}")" http://127.0.0.1:8500/v1/kv/jenkins/pipeline_nomad_deploy_ssh_targets
       '''
   }
   stage('provision') {
     sh '''#!/usr/bin/env bash
       set -xeEo pipefail
       trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
-      SSH_TARGET="$(curl -Ss http://127.0.0.1:8500/v1/kv/jenkins/pipeline_nomad_deploy_ssh_target?raw)"
+      SSH_TARGETS="$(curl -Ss http://127.0.0.1:8500/v1/kv/jenkins/pipeline_nomad_deploy_ssh_targets?raw)"
       SSH_OPTS='-o LogLevel=error -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes'
-      ssh ${SSH_OPTS} ${SSH_TARGET} "sudo yum -q -y install python libselinux-python"
+      for s in ${SSH_TARGETS//,/ }; do
+        ssh ${SSH_OPTS} ${s} "sudo yum -q -y install python libselinux-python"
+      done
       ./apl-wrapper.sh ansible/target-nomad-${ANSIBLE_SCOPE}.yml
     '''
   }
--- origin/jobs/scm/pipeline-jenkins-deploy.groovy
+++ origin/jobs/scm/pipeline-jenkins-deploy.groovy
@@ -17,28 +17,32 @@ node {
         submoduleCfg: [], 
         userRemoteConfigs: [[url: 'https://github.com/alexandruast/platform-inception.git']]])
       sh '''
-        if [[ *"@"* == "${ANSIBLE_TARGET}" ]]; then
-          SSH_TARGET="${ANSIBLE_TARGET}"
+      declare -a SSH_TARGETS
+      for s in ${ANSIBLE_TARGET//,/ }; do
+        if [[ *"@"* == "${s}" ]]; then
+          SSH_TARGETS=("${SSH_TARGETS}" "${s}")
         else
           SSH_USER=$(echo "${ANSIBLE_EXTRAVARS}" | tr "'" '"' | jq -r .ansible_user)
           if [[ "${SSH_USER}" != "" ]]; then
-            SSH_TARGET="${SSH_USER}@${ANSIBLE_TARGET}"
+            SSH_TARGETS=("${SSH_TARGETS}" "${SSH_USER}@${s}")
           else
-            SSH_TARGET="${ANSIBLE_TARGET}"
+            SSH_TARGETS=("${SSH_TARGETS}" "${s}")
           fi
         fi
-        # echo "SSH_TARGET=${SSH_TARGET}" >> .jenkins_env
-        curl -Ss --request PUT --data "${SSH_TARGET}" http://127.0.0.1:8500/v1/kv/jenkins/pipeline_jenkins_deploy_ssh_target
+      done
+      curl -Ss --request PUT --data "$(IFS=$','; echo "${SSH_TARGETS[*]}")" http://127.0.0.1:8500/v1/kv/jenkins/pipeline_jenkins_deploy_ssh_targets
       '''
     }
     stage('provision') {
       sh '''#!/usr/bin/env bash
         set -xeEo pipefail
         trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
-        SSH_TARGET="$(curl -Ss http://127.0.0.1:8500/v1/kv/jenkins/pipeline_jenkins_deploy_ssh_target?raw)"
+        SSH_TARGETS="$(curl -Ss http://127.0.0.1:8500/v1/kv/jenkins/pipeline_jenkins_deploy_ssh_targets?raw)"
         SSH_OPTS='-o LogLevel=error -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes'
         source ./${JENKINS_SCOPE}/.scope
-        ssh ${SSH_OPTS} ${SSH_TARGET} "sudo yum -q -y install python libselinux-python"
+        for s in ${SSH_TARGETS//,/ }; do
+          ssh ${SSH_OPTS} ${s} "sudo yum -q -y install python libselinux-python"
+        done
         ./apl-wrapper.sh ansible/target-${JENKINS_SCOPE}-jenkins.yml
       '''
     }
@@ -46,15 +50,17 @@ node {
       sh '''#!/usr/bin/env bash
         set -xeEo pipefail
         trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
-        SSH_TARGET="$(curl -Ss http://127.0.0.1:8500/v1/kv/jenkins/pipeline_jenkins_deploy_ssh_target?raw)"
+        SSH_TARGETS="$(curl -Ss http://127.0.0.1:8500/v1/kv/jenkins/pipeline_jenkins_deploy_ssh_targets?raw)"
         SSH_CONTROL_SOCKET="/tmp/ssh-control-socket-$(uuidgen)"
-        trap 'ssh -S "${SSH_CONTROL_SOCKET}" -O exit ${SSH_TARGET}' EXIT
+        trap 'for s in ${SSH_TARGETS//,/ }; do ssh -S "${SSH_CONTROL_SOCKET}" -O exit ${s}; done' EXIT
         SSH_OPTS='-o LogLevel=error -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes -o ExitOnForwardFailure=yes'
         source ./${JENKINS_SCOPE}/.scope
-        tunnel_port=$(perl -e 'print int(rand(999)) + 58000')
-        ssh ${SSH_OPTS} -f -N -M -S "${SSH_CONTROL_SOCKET}" -L ${tunnel_port}:127.0.0.1:${JENKINS_PORT} ${SSH_TARGET}
-        JENKINS_ADDR=http://127.0.0.1:${tunnel_port} ./jenkins-setup.sh
-        JENKINS_BUILD_JOB=system-${JENKINS_SCOPE}-job-seed JENKINS_ADDR=http://127.0.0.1:${tunnel_port} ./jenkins-query.sh ./common/jobs/build-simple-job.groovy
+        for s in ${SSH_TARGETS//,/ }; do
+          tunnel_port=$(perl -e 'print int(rand(999)) + 58000')
+          ssh ${SSH_OPTS} -f -N -M -S "${SSH_CONTROL_SOCKET}" -L ${tunnel_port}:127.0.0.1:${JENKINS_PORT} ${s}
+          JENKINS_ADDR=http://127.0.0.1:${tunnel_port} ./jenkins-setup.sh
+          JENKINS_BUILD_JOB=system-${JENKINS_SCOPE}-job-seed JENKINS_ADDR=http://127.0.0.1:${tunnel_port} ./jenkins-query.sh ./common/jobs/build-simple-job.groovy
+        done
       '''
     }
     stage('cleanup') {
