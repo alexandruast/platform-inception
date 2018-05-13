2928ac40ceea8dab01d9ce74bbfabcf1c812961e
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-13 18:24:55 +0300

    Work in progress

--- origin/jobs/scm/pipeline-jenkins-deploy.groovy
+++ origin/jobs/scm/pipeline-jenkins-deploy.groovy
@@ -1,5 +1,4 @@
 node {
-  properties([disableConcurrentBuilds()])
   wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs:[
     [password: "params.JENKINS_ADMIN_PASS", var: 'JENKINS_ADMIN_PASS']
   ]]) {
