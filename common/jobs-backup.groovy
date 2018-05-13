6109ae591a2766187cfc355926a49efc1a81619b
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-13 21:34:08 +0300

    Work in progress

--- common/jobs-backup.groovy
+++ common/jobs-backup.groovy
@@ -2,5 +2,5 @@ import jenkins.model.Jenkins
 println new ProcessBuilder(
   'sh',
   '-c',
-  'tar -cpf /tmp/jenkins_backup.tar.gz --exclude=config.xml --one-file-system -C {{JENKINS_HOME}} ./jobs'
+  'tar -cpf /tmp/jenkins_backup.tar --exclude=config.xml --one-file-system -C {{JENKINS_HOME}} ./jobs ./workspace'
 ).redirectErrorStream(true).start().text
--- common/jobs-restore.groovy
+++ common/jobs-restore.groovy
@@ -2,5 +2,5 @@ import jenkins.model.Jenkins
 println new ProcessBuilder(
   'sh',
   '-c',
-  'tar -xpf /tmp/jenkins_backup.tar.gz -C {{JENKINS_HOME}} --numeric-owner'
+  'tar -xpf /tmp/jenkins_backup.tar -C {{JENKINS_HOME}} --numeric-owner'
 ).redirectErrorStream(true).start().text
