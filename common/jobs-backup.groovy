1a9dc9e879e70afb19943914b9e65cfc67327571
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-13 19:14:21 +0300

    Work in progress

--- common/jobs-backup.groovy
+++ common/jobs-backup.groovy
@@ -1,5 +1,6 @@
 import jenkins.model.Jenkins
-def sout = new StringBuilder(), serr = new StringBuilder()
-def proc = 'tar -cpzf /tmp/jenkins_backup.tar.gz --exclude=config.xml --one-file-system -C {{JENKINS_HOME}} ./jobs ./workspace'.execute()
-proc.consumeProcessOutput(sout, serr)
-proc.waitForOrKill(4 * 60 * 1000)
+println new ProcessBuilder(
+  'sh',
+  '-c',
+  'tar -cpf /tmp/jenkins_backup.tar.gz --exclude=config.xml --one-file-system -C {{JENKINS_HOME}} ./jobs'
+).redirectErrorStream(true).start().text
--- common/jobs-restore.groovy
+++ common/jobs-restore.groovy
@@ -1,5 +1,6 @@
 import jenkins.model.Jenkins
-def sout = new StringBuilder(), serr = new StringBuilder()
-def proc = 'tar -xpzf /tmp/jenkins_backup.tar.gz -C {{JENKINS_HOME}} --numeric-owner'.execute()
-proc.consumeProcessOutput(sout, serr)
-proc.waitForOrKill(4 * 60 * 1000)
+println new ProcessBuilder(
+  'sh',
+  '-c',
+  'tar -xpf /tmp/jenkins_backup.tar.gz -C {{JENKINS_HOME}} --numeric-owner'
+).redirectErrorStream(true).start().text
