2f7b6462754f9beb0dc001ecef60515de60bc224
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-13 18:41:37 +0300

    Work in progress

--- common/jobs/scm/pipeline-vault-deploy.groovy
+++ common/jobs/scm/pipeline-vault-deploy.groovy
@@ -13,9 +13,12 @@ node {
       doGenerateSubmoduleConfigurations: false, 
       submoduleCfg: [], 
       userRemoteConfigs: [[url: 'https://github.com/alexandruast/platform-inception.git']]])
-      sh '''#!/usr/bin/env bash
+  }
+  stage('deploy') {
+    sh '''#!/usr/bin/env bash
       set -xeEo pipefail
       trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
+      SSH_OPTS='-o LogLevel=error -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes'
       declare -a SSH_TARGETS=()
       for s in ${ANSIBLE_TARGET//,/ }; do
         if [[ *"@"* == "${s}" ]]; then
@@ -29,17 +32,7 @@ node {
           fi
         fi
       done
-      SSH_TARGETS="$(echo "${SSH_TARGETS[*]}")"
-      echo "SSH_TARGETS=${SSH_TARGETS/ /,}" >> .environment
-      '''
-  }
-  stage('deploy') {
-    sh '''#!/usr/bin/env bash
-      set -xeEo pipefail
-      trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
-      source .environment
-      SSH_OPTS='-o LogLevel=error -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes'
-      for s in ${SSH_TARGETS/,/ }; do
+      for s in ${SSH_TARGETS[@]}; do
         ssh ${SSH_OPTS} ${s} "sudo yum -q -y install python libselinux-python"
       done
       ./apl-wrapper.sh ansible/target-vault-server.yml
