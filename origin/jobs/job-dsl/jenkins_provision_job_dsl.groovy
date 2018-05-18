3fd708aeb3245b84ad01a7d6d527a9773596be89
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-18 12:52:14 +0300

    Work in progress

--- origin/jobs/job-dsl/jenkins_provision_job_dsl.groovy
+++ origin/jobs/job-dsl/jenkins_provision_job_dsl.groovy
@@ -1,7 +1,8 @@
 def scopes = ['factory', 'prod']
-def jobSuffix='jenkins-deploy'
+def jobSuffix='provision'
+def jobPrefix='jenkins'
 scopes.each { scope ->
-  pipelineJob("${scope}-${jobSuffix}") {
+  pipelineJob("${jobPrefix}-${scope}-${jobSuffix}") {
     description("Dynamically generated with job-dsl by ${JOB_NAME}\nAny changes to this item will be overwritten without notice.")
     def repo = 'https://github.com/alexandruast/platform-inception'
     keepDependencies(false)
