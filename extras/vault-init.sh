de92d17e8877928f99f7a3c2d3da3f9097fb12ba
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-20 14:07:20 +0300

    Work in progress

--- extras/vault-init.sh
+++ extras/vault-init.sh
@@ -27,13 +27,6 @@ vault_reset() {
   sleep 0.25
   echo "[info] vault servers unsealed"
   
-  # enable approle backend
-  curl -Ssf -X POST \
-    -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
-    -d "{\"type\":\"approle\"}" \
-    ${VAULT_ADDR}/v1/sys/auth/approle
-  echo "[info] vault enabled approle backend"
-  
   # enable syslog backend
   curl -Ssf -X PUT \
     -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
@@ -41,95 +34,102 @@ vault_reset() {
     ${VAULT_ADDR}/v1/sys/audit/syslog
   echo "[info] vault enabled syslog audit backend"
   
-  curl --silent -X PUT -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" -d '{"value":"HelloWorldSecret"}' ${VAULT_ADDR}/v1/secret/hello
-  echo "[info] vault written hello secret"
+  # enable approle backend
+  curl -Ssf -X POST \
+    -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
+    -d "{\"type\":\"approle\"}" \
+    ${VAULT_ADDR}/v1/sys/auth/approle
+  echo "[info] vault enabled approle backend"
   
-  # importing app-f85b911a policy
-  policy_name='app-f85b911a'
-  policy_string=$(cat vault/policies/${policy_name}.json | jq -c . | sed 's/"/\\\"/g')
-  curl -X PUT \
-    -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
-    -d "{\"policy\":\"${policy_string}\"}" ${VAULT_ADDR}/v1/sys/policy/${policy_name}
+  # importing policies
+  for policy_name in admin provisioner operations jenkins; do
+    policy_string=$(cat vault/policies/${policy_name}.json | jq -c . | sed 's/"/\\\"/g')
+    curl -Ssf -X PUT \
+      -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
+      -d "{\"policy\":\"${policy_string}\"}" ${VAULT_ADDR}/v1/sys/policy/${policy_name}
+  done
+  echo "[info] vault policies imported"
   
-  # importing app-admin policy
-  policy_name='app-admin'
-  policy_string=$(cat vault/policies/${policy_name}.json | jq -c . | sed 's/"/\\\"/g')
-  curl -X PUT \
+  # curl -Ssf -X GET \
+  #   -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
+  #   ${VAULT_ADDR}/v1/sys/policy/admin | jq
+  
+  # get admin token, to be used from now on
+  VAULT_ADMIN_TOKEN="$(curl -Ssf -X POST \
     -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
-    -d "{\"policy\":\"${policy_string}\"}" ${VAULT_ADDR}/v1/sys/policy/${policy_name}
-    
-  # importing app-read policy
-  policy_name='app-read'
-  policy_string=$(cat vault/policies/${policy_name}.json | jq -c . | sed 's/"/\\\"/g')
-  curl -X PUT \
-    -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
-    -d "{\"policy\":\"${policy_string}\"}" ${VAULT_ADDR}/v1/sys/policy/${policy_name}
+    -d "{\"policies\":[\"admin\"]}" \
+    ${VAULT_ADDR}/v1/auth/token/create | jq -re .auth.client_token)"
 
-  curl --silent -X PUT \
-    -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
-    -d "{\"secret_id_ttl\":\"5m\",\"token_ttl\":\"5m\",\"token_max_ttl\":\"10m\",\"policies\":[\"app-f85b911a\"]}" \
-    ${VAULT_ADDR}/v1/auth/approle/role/app-f85b911a
-  echo "[info] vault policies written"
-  
-  APP_ADMIN_VAULT_TOKEN="$(curl --silent -X POST \
-    -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
-    -d "{\"policies\":[\"app-admin\"]}" \
+  curl -Ssf -X PUT \
+    -H "X-Vault-Token:${VAULT_ADMIN_TOKEN}" \
+    -d "{\"secret_id_ttl\":\"5m\",\"token_ttl\":\"5m\",\"token_max_ttl\":\"10m\",\"policies\":[\"jenkins\"]}" \
+    ${VAULT_ADDR}/v1/auth/approle/role/jenkins
+
+  echo "[info] vault approles defined"
+
+  OPERATIONS_VAULT_TOKEN="$(curl -Ssf -X POST \
+    -H "X-Vault-Token:${VAULT_ADMIN_TOKEN}" \
+    -d "{\"policies\":[\"operations\"]}" \
     ${VAULT_ADDR}/v1/auth/token/create | jq -re .auth.client_token)"
   
-  APP_F85B911A_VAULT_ROLE_ID="$(curl --silent -X GET \
-    -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
-    ${VAULT_ADDR}/v1/auth/approle/role/app-f85b911a/role-id | jq -re .data.role_id)"
-  
-  APP_READ_VAULT_TOKEN="$(curl --silent -X POST \
-    -H "X-Vault-Token:${VAULT_ROOT_TOKEN}" \
-    -d "{\"policies\":[\"app-read\"]}" \
+  JENKINS_VAULT_TOKEN="$(curl -Ssf -X POST \
+    -H "X-Vault-Token:${VAULT_ADMIN_TOKEN}" \
+    -d "{\"policies\":[\"jenkins\"]}" \
     ${VAULT_ADDR}/v1/auth/token/create | jq -re .auth.client_token)"
+
+  JENKINS_VAULT_ROLE_ID="$(curl -Ssf -X GET \
+    -H "X-Vault-Token:${VAULT_ADMIN_TOKEN}" \
+    ${VAULT_ADDR}/v1/auth/approle/role/jenkins/role-id | jq -re .data.role_id)"
+
+  echo "[info] VAULT_ADMIN_TOKEN: ${VAULT_ADMIN_TOKEN}"
+  echo "[info] OPERATIONS_VAULT_TOKEN: ${OPERATIONS_VAULT_TOKEN}"
+  echo "[info] JENKINS_VAULT_TOKEN: ${JENKINS_VAULT_TOKEN}"
+  echo "[info] JENKINS_VAULT_ROLE_ID: ${JENKINS_VAULT_ROLE_ID}"
   
-  echo "[info] APP_ADMIN_VAULT_TOKEN: ${APP_ADMIN_VAULT_TOKEN}"
-  echo "[info] APP_F85B911A_VAULT_ROLE_ID: ${APP_F85B911A_VAULT_ROLE_ID}"
-  echo "[info] APP_READ_VAULT_TOKEN: ${APP_READ_VAULT_TOKEN}"
-  
-  token_renew_seconds=$(curl --silent -X POST \
-    -H "X-Vault-Token:${APP_ADMIN_VAULT_TOKEN}" \
+  token_renew_seconds=$(curl -Ssf -X POST \
+    -H "X-Vault-Token:${JENKINS_VAULT_TOKEN}" \
     -d "{\"increment\": \"96h\"}" \
     "${VAULT_ADDR}/v1/auth/token/renew-self" | jq -re .auth.lease_duration)
   echo "[info] test self token renewal passed, seconds=${token_renew_seconds}"
   
   # generate secret-id for approle and wrap it (will transfer to app at deploy)
-  approle_secid_unwrap_token="$(curl --silent -X POST \
-    -H "X-Vault-Token:${APP_ADMIN_VAULT_TOKEN}" \
+  approle_secid_unwrap_token="$(curl -Ssf -X POST \
+    -H "X-Vault-Token:${JENKINS_VAULT_TOKEN}" \
     -H "X-Vault-Wrap-TTL:60" \
-    ${VAULT_ADDR}/v1/auth/approle/role/app-f85b911a/secret-id | jq -re .wrap_info.token)"
+    ${VAULT_ADDR}/v1/auth/approle/role/jenkins/secret-id | jq -re .wrap_info.token)"
   
   # unwrap secret-id (on the application side)
-  approle_vault_secret_id="$(curl --silent -X POST \
+  approle_vault_secret_id="$(curl -Ssf -X POST \
     -H "X-Vault-Token:${approle_secid_unwrap_token}" \
     ${VAULT_ADDR}/v1/sys/wrapping/unwrap | jq -re .data.secret_id)"
   
-  # unwrapping a second time should fail
-  ! curl --silent -X POST \
+  # unwrapping a second time should fail, not using curl -f
+  ! curl -Ss -X POST \
     -H "X-Vault-Token:${approle_secid_unwrap_token}" \
     ${VAULT_ADDR}/v1/sys/wrapping/unwrap | jq -re .data.secret_id >/dev/null
   
   # get approle token (on the application side)
-  approle_token="$(curl --silent -X POST \
+  approle_token="$(curl -Ssf -X POST \
     -H "X-Vault-Token:${approle_vault_secret_id}" \
-    -d "{\"role_id\":\"${APP_F85B911A_VAULT_ROLE_ID}\",\"secret_id\":\"${approle_vault_secret_id}\"}" \
+    -d "{\"role_id\":\"${JENKINS_VAULT_ROLE_ID}\",\"secret_id\":\"${approle_vault_secret_id}\"}" \
     ${VAULT_ADDR}/v1/auth/approle/login | jq -re .auth.client_token)"
+
+  curl -Ssf -X PUT -H "X-Vault-Token:${VAULT_ADMIN_TOKEN}" -d '{"value":"bar"}' ${VAULT_ADDR}/v1/secret/operations/foo
+  echo "[info] vault written operations/foo secret"
   
   # use approle token to read secret (on the application side)
-  secret="$(curl --silent -X GET \
-    -H "X-Vault-Token:$approle_token" \
-    "$VAULT_ADDR/v1/secret/hello" | jq -re .data.value)"
+  secret="$(curl -Ssf -X GET \
+    -H "X-Vault-Token:${approle_token}" \
+    "${VAULT_ADDR}/v1/secret/operations/foo" | jq -re .data.value)"
   echo "[info] vault token wrap/unwrap tests passed, secret=$secret"
   
-  JENKINS_CREDENTIAL_ID="APP_READ_VAULT_TOKEN" \
+  JENKINS_CREDENTIAL_ID="JENKINS_VAULT_TOKEN" \
     JENKINS_CREDENTIAL_DESCRIPTION="Vault Token" \
-    JENKINS_CREDENTIAL_SECRET="${APP_READ_VAULT_TOKEN}" \
+    JENKINS_CREDENTIAL_SECRET="${JENKINS_VAULT_TOKEN}" \
     ./jenkins-query.sh common/credential-update.groovy
-  JENKINS_CREDENTIAL_ID="APP_F85B911A_VAULT_ROLE_ID" \
+  JENKINS_CREDENTIAL_ID="JENKINS_VAULT_ROLE_ID" \
     JENKINS_CREDENTIAL_DESCRIPTION="Vault Role ID" \
-    JENKINS_CREDENTIAL_SECRET="${APP_F85B911A_VAULT_ROLE_ID}" \
+    JENKINS_CREDENTIAL_SECRET="${JENK_VAULT_ROLE_ID}" \
     ./jenkins-query.sh common/credential-update.groovy
 }
 
