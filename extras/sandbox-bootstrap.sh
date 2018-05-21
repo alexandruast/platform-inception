2613911d222ccd0f6bda07f92cd44f022180c93e
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-21 07:15:29 +0300

    Work in progress

--- extras/sandbox-bootstrap.sh
+++ extras/sandbox-bootstrap.sh
@@ -5,18 +5,22 @@ trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
 ci_admin_pass=$1
 sandbox_ip=$2
 
+install_pip() {
+  curl -LSs "https://bootstrap.pypa.io/get-pip.py" | sudo python
+}
+
+install_ansible() {
+  sudo pip install ansible==2.5.2
+}
+
 install_jq() {
   sudo curl -LSs https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -o /usr/local/bin/jq \
   && sudo chmod +x /usr/local/bin/jq
 }
 
-install_pip() {
-  curl -LSs "https://bootstrap.pypa.io/get-pip.py" | sudo python
-}
-
 sudo yum -q -y install python libselinux-python
 which pip >/dev/null || install_pip
-sudo pip install ansible==2.5.2
+which ansible >/dev/null || install_ansible
 which jq >/dev/null || install_jq
 
 cd /vagrant/
