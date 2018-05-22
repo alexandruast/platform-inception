dbb1b4b5e7d26bc995d1885d11a8b52486d7d038
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-22 20:06:08 +0300

    Work in progress

--- common/jobs/scm/consul-data-import-pipeline.groovy
+++ common/jobs/scm/consul-data-import-pipeline.groovy
@@ -12,7 +12,18 @@ node {
     sh '''#!/usr/bin/env bash
     set -xeuEo pipefail
     trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
-    exit 0
+    REGISTRY_ADDRESS="$(curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-settings/docker_registry_address?raw)"
+    REGISTRY_PATH="$(curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-settings/docker_registry_path?raw)"
+    ansible all -i localhost, --connection=local -m template -a "src=config/main.yml.j2 dest=config/main.yml" >/dev/null
+    TAG_VERSION="$(
+      curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-data/qa/yaml-to-consul/tag_version?raw || \
+      curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-data/integration/yaml-to-consul/tag_version?raw || \
+      curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-data/sandbox/yaml-to-consul/tag_version?raw
+    )"
+    docker run --rm \
+      -v "$(pwd)/config:/config" \
+      -v "$(pwd)/import:/import" \
+      ${REGISTRY_ADDRESS}/$REGISTRY_PATH/yaml-to-consul:${TAG_VERSION}
     '''
   }
   stage('cleanup') {
