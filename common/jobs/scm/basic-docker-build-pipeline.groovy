8b121cb9f47de649b1c95782f73fb92a8f6caa21
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-24 08:46:13 +0300

    Work in progress

--- common/jobs/scm/basic-docker-build-pipeline.groovy
+++ common/jobs/scm/basic-docker-build-pipeline.groovy
@@ -37,7 +37,7 @@ node {
       docker login "${REGISTRY_ADDRESS}" --username="${REGISTRY_USERNAME}" --password-stdin <<< ${REGISTRY_PASSWORD} >/dev/null
       BUILD_DIR="$(curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-settings/${PLATFORM_ENVIRONMENT}/${POD_NAME}/build_dir?raw)"
       cd "${WORKSPACE}/${BUILD_DIR}"
-      COMPOSE_FILE="version: '3'\nservices:\n\t${POD_NAME}:\n\t\timage: '${REGISTRY_ADDRESS}/${REGISTRY_PATH}/${POD_NAME}:${POD_TAG}'\n\t\tbuild: './'"
+      COMPOSE_FILE="version: '3'\nservices:\n\t${POD_NAME}:\n\t\timage: ${REGISTRY_ADDRESS}/${REGISTRY_PATH}/${POD_NAME}:${POD_TAG}\n\t\tbuild: ./"
       docker-compose --project-name "${POD_NAME}-${POD_TAG}" --no-ansi -f - build <<< ${COMPOSE_FILE}
       docker-compose --project-name "${POD_NAME}-${POD_TAG}" --no-ansi -f - push  <<< ${COMPOSE_FILE}
       curl -Ssf -X PUT -d "${POD_TAG}" ${CONSUL_HTTP_ADDR}/v1/kv/platform-data/${PLATFORM_ENVIRONMENT}/${POD_NAME}/tag_version >/dev/null
