b961d786aa60707b4403f8e383c58cd6d6515e0e
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-26 17:20:08 +0300

    Work in progress

--- common/jobs/scm/basic-compose-pod-pipeline.groovy
+++ common/jobs/scm/basic-compose-pod-pipeline.groovy
@@ -14,17 +14,13 @@ node {
         string(credentialsId: 'JENKINS_VAULT_ROLE_ID', variable: 'VAULT_ROLE_ID'),
     ]) {
       sh '''
-      curl -LSs https://raw.githubusercontent.com/alexandruast/platform-inception/devel/extras/compose-pod-build.sh > compose-pod-build.sh
-      chmod +x compose-pod-build.sh
-      ./compose-pod-build.sh
+      extras/compose-pod-build.sh
       '''
     }
   }
   stage('deploy') {
     sh '''
-      curl -LSs https://raw.githubusercontent.com/alexandruast/platform-inception/devel/extras/compose-pod-deploy.sh > compose-pod-deploy.sh
-      chmod +x compose-pod-deploy.sh
-      ./compose-pod-deploy.sh
+      extras/compose-pod-build.sh
     '''
   }
   stage('cleanup') {
