49d9ade642e0dcdd353b2cd57331fc0e02b530d6
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-26 16:21:31 +0300

    Work in progress

--- extras/compose-pod-build.sh
+++ extras/compose-pod-build.sh
@@ -2,67 +2,128 @@
 set -xeEuo pipefail
 trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
 
-readonly VAULT_ADDR="$(curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/vault_address?raw)"
+readonly AUTO_COMPOSE_TEMPLATE="$(cat << EOF
+version: '3'
+  services:
+    {{lookup('env','POD_NAME')}}:
+      image: {{lookup('env','REGISTRY_ADDRESS')}}/{{lookup('env','REGISTRY_PATH')}}/{{lookup('env','POD_NAME')}}:{{lookup('env','BUILD_TAG')}}
+      build: ./
+EOF
+)"
 
-readonly CHECKOUT_COMMIT_ID="$(curl -Ssf http://127.0.0.1:8500/v1/kv/${PLATFORM_ENVIRONMENT}/${POD_NAME}/checkout_commit_id?raw)"
-readonly POD_TAG="${CHECKOUT_COMMIT_ID:0:7}"
+readonly AUTO_NOMAD_TEMPLATE="$(cat << EOF
+job "{{lookup('env','POD_NAME')}}" {
+  datacenters = ["dc1"]
+  type = "service"
+  update {
+    max_parallel = 1
+  }
+  group "{{lookup('env','POD_NAME')}}" {
+    task "{{lookup('env','POD_NAME')}}-{{lookup('env','BUILD_TAG')}}" {
+      driver = "docker"
+      config {
+        image = "{{lookup('env','REGISTRY_ADDRESS')}}/{{lookup('env','REGISTRY_PATH')}}/fabio:{{lookup('env','BUILD_TAG')}}"
+        auth {
+          server_address = "{{lookup('env','REGISTRY_ADDRESS')}}"
+          username = "{{lookup('env','REGISTRY_USERNAME')}}"
+          password = "{{lookup('env','REGISTRY_PASSWORD')}}"
+        }
+      }
+      resources {
+        memory = 128
+        network {
+          mbits = 100
+        }
+      }
+      service {
+        name = "{{lookup('env','POD_NAME')}}"
+      }
+    }
+  }
+}
+EOF
+)"
 
-readonly PREVIOUS_BUILD_TAG="$(curl -Ss ${CONSUL_HTTP_ADDR}/v1/kv/platform-data/${PLATFORM_ENVIRONMENT}/${POD_NAME}/build_tag?raw)"
+readonly VAULT_ADDR="$(curl -Ssf \
+  ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/vault_address?raw)"
 
-if [[ "${POD_TAG}" == "${PREVIOUS_BUILD_TAG}" ]]; then
-  echo [warning] commit id is the same, will not build again!
+readonly BUILD_TAG="$(git rev-parse --short HEAD)"
+
+readonly PREV_BUILD_TAG="$(curl -Ss \
+  ${CONSUL_HTTP_ADDR}/v1/kv/platform-data/${PLATFORM_ENVIRONMENT}/${POD_NAME}/build_tag?raw)"
+
+if [[ "${BUILD_TAG}" == "${PREV_BUILD_TAG}" ]]; then
+  echo "[warning] commit id is the same, will not build again!"
   exit 0
 fi
 
-readonly REGISTRY_ADDRESS="$(curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/docker_registry_address?raw)"
-readonly REGISTRY_PATH="$(curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/docker_registry_path?raw)"
+readonly REGISTRY_ADDRESS="$(curl -Ssf \
+  ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/docker_registry_address?raw)"
+
+readonly REGISTRY_PATH="$(curl -Ssf \
+  ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/docker_registry_path?raw)"
 
 readonly REGISTRY_CREDENTIALS="$(curl -Ssf -X GET \
   -H "X-Vault-Token:${VAULT_TOKEN}" \
   "${VAULT_ADDR}/v1/secret/operations/docker-registry" | jq -re .data.value)"
+
 readonly REGISTRY_USERNAME="${REGISTRY_CREDENTIALS%:*}"
 readonly REGISTRY_PASSWORD="${REGISTRY_CREDENTIALS#*:}"
 
-readonly BUILD_DIR="$(curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/build_dir?raw)"
+readonly BUILD_DIR="$(curl -Ssf \
+  ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/build_dir?raw)"
 
-readonly BASE_COMPOSE_TEMPLATE="${WORKSPACE}/${BUILD_DIR}/docker-compose-base.yml.j2"
+readonly COMPOSE_FILE="${WORKSPACE}/${BUILD_DIR}/docker-compose.yml"
+if [[ ! -f "${COMPOSE_FILE}" ]] && [[ ! -f "${COMPOSE_FILE}.j2" ]]; then
+  echo "${AUTO_COMPOSE_TEMPLATE}" > "${COMPOSE_FILE}.j2"
+fi
 
-cat << EOF > "${BASE_COMPOSE_FILE}"
-version: '3'
-  services:
-    {{ POD_NAME }}:
-      image: {{ REGISTRY_ADDRESS }}/{{ REGISTRY_PATH }}/{{ POD_NAME }}:{{ POD_TAG }}
-      build: ./
-EOF
-
-readonly BASE_COMPOSE_FILE="${BASE_COMPOSE_TEMPLATE%%.j2}"
-
-readonly EXPECTED_COMPOSE_FILE="${WORKSPACE}/${BUILD_DIR}/docker-compose.yml"
+readonly NOMAD_FILE="${WORKSPACE}/${BUILD_DIR}/nomad-job.hcl"
+if [[ ! -f "${NOMAD_FILE}" ]] && [[ ! -f "${NOMAD_FILE}.j2" ]]; then
+  echo "${AUTO_NOMAD_TEMPLATE}" > "${NOMAD_FILE}.j2"
+fi
 
 export REGISTRY_ADDRESS
 export REGISTRY_USERNAME
 export REGISTRY_PASSWORD
 export REGISTRY_PATH
 export POD_NAME
-export POD_TAG
+export BUILD_TAG
 
+# Parsing all jinja2 templates
 while IFS='' read -r -d '' f; do
-  ansible all -i localhost, --connection=local -m template -a "src=${f} dest=${f%%.j2}"
+  ansible all -i localhost, \
+    --connection=local \
+    -m template \
+    -a "src=${f} dest=${f%%.j2}"
 done < <(find "${WORKSPACE}/${BUILD_DIR}" -type f -name '*.j2' -print0)
 
-nomad validate "${WORKSPACE}/${BUILD_DIR}/nomad-job.hcl"
-nomad run -output "${WORKSPACE}/${BUILD_DIR}/nomad-job.hcl" > "${WORKSPACE}/${BUILD_DIR}/nomad-job.json"
+nomad validate \
+  "${WORKSPACE}/${BUILD_DIR}/nomad-job.hcl"
 
-compose_file="${EXPECTED_COMPOSE_FILE}"
+nomad run \
+  -output "${WORKSPACE}/${BUILD_DIR}/nomad-job.hcl" > "${WORKSPACE}/${BUILD_DIR}/nomad-job.json"
 
-[[ -f "${EXPECTED_COMPOSE_FILE}" ]] || compose_file="${BASE_COMPOSE_FILE}"
-
-trap 'docker-compose -f "${compose_file}" --project-name "${POD_NAME}-${POD_TAG}" down -v --rmi all --remove-orphans' EXIT
+trap 'docker-compose --project-name "${POD_NAME}-${BUILD_TAG}" down -v --rmi all --remove-orphans' EXIT
 
 docker login "${REGISTRY_ADDRESS}" \
   --username="${REGISTRY_USERNAME}" \
   --password-stdin <<< ${REGISTRY_PASSWORD} >/dev/null
 
-docker-compose -f "${compose_file}" --project-name "${POD_NAME}-${POD_TAG}" --no-ansi build --no-cache
-docker-compose -f "${compose_file}" --project-name "${POD_NAME}-${POD_TAG}" --no-ansi push
-curl -Ssf -X PUT -d "${POD_TAG}" ${CONSUL_HTTP_ADDR}/v1/kv/platform-data/${PLATFORM_ENVIRONMENT}/${POD_NAME}/build_tag >/dev/null
+cd "${WORKSPACE}/${BUILD_DIR}"
+
+docker-compose \
+  --project-name "${POD_NAME}-${BUILD_TAG}" \
+  --no-ansi \
+  --no-cache \
+  build
+
+docker-compose \
+  --project-name "${POD_NAME}-${BUILD_TAG}" \
+  --no-ansi \
+  push
+
+curl -Ssf -X PUT \
+  -d "${BUILD_TAG}" \
+  ${CONSUL_HTTP_ADDR}/v1/kv/platform-data/${PLATFORM_ENVIRONMENT}/${POD_NAME}/build_tag >/dev/null
+
---
+++ extras/compose-pod-deploy.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -xeEuo pipefail
+trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
+
+readonly SSH_OPTS='-o LogLevel=error \
+  -o StrictHostKeyChecking=no \
+  -o UserKnownHostsFile=/dev/null \
+  -o BatchMode=yes \
+  -o ExitOnForwardFailure=yes'
+
+readonly SSH_DEPLOY_ADDRESS="$(curl -Ssf \
+  ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/ssh_deploy_address?raw)"
+
+trap 'ssh -S "${WORKSPACE}/ssh-control-socket" -O exit ${SSH_DEPLOY_ADDRESS}' EXIT
+
+# Creating an SSH tunnel to the nomad server
+TUNNEL_PORT=$(perl -e 'print int(rand(999)) + 58000')
+ssh ${SSH_OPTS} -f -N -M -S "${WORKSPACE}/ssh-control-socket" -L ${TUNNEL_PORT}:127.0.0.1:4646 ${SSH_DEPLOY_ADDRESS}
+NOMAD_ADDR=http://127.0.0.1:${TUNNEL_PORT}
+
+# Try job planning, so we can catch any issues before actually deploying stuff
+JOB_PLAN_DATA="$(curl -Ssf -X POST \
+  -d @nomad-job.json \
+  ${NOMAD_ADDR}/v1/job/${POD_NAME}/plan)"
+
+# To go further with the deploy, the FailedTGAllocs field must be null
+FAILED_ALLOCS="$(echo "${JOB_PLAN_DATA}" \
+  | grep 'FailedTGAllocs' \
+  | jq -rc .FailedTGAllocs)"
+[[ "${FAILED_ALLOCS}" == "null" ]]
+
+# Posting job data
+JOB_POST_DATA="$(curl -Ssf -X POST -d @nomad-job.json ${NOMAD_ADDR}/v1/jobs)"
+
+# Getting information
+JOB_EVAL_ID="$(echo "${JOB_POST_DATA}" \
+  | jq -re .EvalID)"
+
+DEPLOYMENT_ID="$(curl -Ssf \
+  ${NOMAD_ADDR}/v1/evaluation/${JOB_EVAL_ID} \
+  | jq -re .DeploymentID)"
+
+# Infinite loop until status is failed or successful
+while :; do
+  sleep 10 &
+  wait || true
+  DEPLOYMENT_STATUS="$(curl -Ssf ${NOMAD_ADDR}/v1/deployment/${DEPLOYMENT_ID} | jq -re .Status)"
+  case "${DEPLOYMENT_STATUS}" in
+    successful)
+      curl -Ssf -X PUT -d "${POD_TAG}" ${CONSUL_HTTP_ADDR}/v1/kv/platform-data/${PLATFORM_ENVIRONMENT}/${POD_NAME}/deploy_tag >/dev/null
+      exit 0
+    ;;
+    failed)
+      exit 1
+  esac
+done
+exit 1
+
