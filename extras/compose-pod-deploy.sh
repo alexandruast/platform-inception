c8e1eaae974a4dc3832b73a0d0c0712634e1f9fc
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-26 21:56:32 +0300

    Work in progress

--- extras/compose-pod-deploy.sh
+++ extras/compose-pod-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eEuo pipefail
+set -xeEuo pipefail
 trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
 
 echo "[info] getting all information required for the deploy to start..."
