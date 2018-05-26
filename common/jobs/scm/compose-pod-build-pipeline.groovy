11609cb71e38c7bf4659cac61e88834baefb24e7
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-26 22:36:55 +0300

    Work in progress

--- common/jobs/scm/compose-pod-build-pipeline.groovy
+++ common/jobs/scm/compose-pod-build-pipeline.groovy
@@ -13,12 +13,13 @@ node {
         string(credentialsId: 'JENKINS_VAULT_TOKEN', variable: 'VAULT_TOKEN'),
         string(credentialsId: 'JENKINS_VAULT_ROLE_ID', variable: 'VAULT_ROLE_ID'),
     ]) {
-      gitBranch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/scm_branch?raw").trim()
-      gitURL = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/scm_url?raw").trim()
+      gitBranch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/shell/scm_branch?raw").trim()
+      gitURL = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/shell/scm_url?raw").trim()
+      gitDIR = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/shell/scm_dir?raw").trim()
       checkout_info = checkout([$class: 'GitSCM',
         branches: [[name: gitBranch]],
         doGenerateSubmoduleConfigurations: false,
-        extensions: [[$class: 'SparseCheckoutPaths', sparseCheckoutPaths:[[$class:'SparseCheckoutPath', path:'common/jobs/shell']]]],
+        extensions: [[$class: 'SparseCheckoutPaths', sparseCheckoutPaths:[[$class: 'SparseCheckoutPath', path: scm_dir]]]],
         submoduleCfg: [],
         userRemoteConfigs: [[url: gitURL]]])
       sh('compose-pod-build.sh')
