41e248d3540a2475b7f0d99f6fa57c2a4bf6cd79
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-26 22:30:17 +0300

    Work in progress

--- common/jobs/scm/compose-pod-build-pipeline.groovy
+++ common/jobs/scm/compose-pod-build-pipeline.groovy
@@ -13,12 +13,15 @@ node {
         string(credentialsId: 'JENKINS_VAULT_TOKEN', variable: 'VAULT_TOKEN'),
         string(credentialsId: 'JENKINS_VAULT_ROLE_ID', variable: 'VAULT_ROLE_ID'),
     ]) {
-      sh '''
-        # extras/compose-pod-build.sh
-        curl -LSs https://raw.githubusercontent.com/alexandruast/platform-inception/devel/extras/compose-pod-build.sh > ./compose-pod-build.sh
-        chmod +x ./compose-pod-build.sh
-        ./compose-pod-build.sh
-      '''
+      gitBranch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/scm_branch?raw").trim()
+      gitURL = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/scm_url?raw").trim()
+      checkout_info = checkout([$class: 'GitSCM',
+        branches: [[name: gitBranch]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [[$class: 'SparseCheckoutPaths', sparseCheckoutPaths:[[$class:'SparseCheckoutPath', path:'common/jobs/shell']]]],
+        submoduleCfg: [],
+        userRemoteConfigs: [[url: gitURL]]])
+      sh('compose-pod-build.sh')
     }
   }
   stage('cleanup') {
