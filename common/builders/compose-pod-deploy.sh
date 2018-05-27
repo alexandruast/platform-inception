ee75a54c181d66aa7a02c901784708d5b859a8a1
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-27 12:43:14 +0300

    Work in progress

--- common/builders/compose-pod-deploy.sh
+++ common/builders/compose-pod-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -xeEuo pipefail
+set -eEuo pipefail
 trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
 
 LOCAL_DIR="$(cd "$(dirname $0)" && pwd)"
