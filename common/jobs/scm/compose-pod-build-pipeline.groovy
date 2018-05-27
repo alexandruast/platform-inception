4f47d9dd049a69072fc9ee87a7360d15c6f034f9
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-27 10:31:48 +0300

    Work in progress

--- common/jobs/scm/compose-pod-build-pipeline.groovy
+++ common/jobs/scm/compose-pod-build-pipeline.groovy
@@ -1,13 +1,13 @@
 node {
   stage('checkout') {
-    scm_branch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/scm_branch?raw").trim()
     scm_url = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/scm_url?raw").trim()
+    scm_branch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/scm_branch?raw").trim()
     checkout_dir = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/checkout_dir?raw").trim()
     checkout_info = checkout([$class: 'GitSCM',
       branches: [[name: scm_branch]],
       doGenerateSubmoduleConfigurations: false,
       extensions:[
-        [$class: 'SparseCheckoutPaths', sparseCheckoutPaths:[[$class: 'SparseCheckoutPath', path: "${checkout_dir}/"]]]
+        [$class: 'SparseCheckoutPaths', sparseCheckoutPaths:[[$class: 'SparseCheckoutPath', path: ""]]]
       ],
       submoduleCfg: [],
       userRemoteConfigs: [[url: scm_url]]]
@@ -18,8 +18,8 @@ node {
         string(credentialsId: 'JENKINS_VAULT_TOKEN', variable: 'VAULT_TOKEN'),
         string(credentialsId: 'JENKINS_VAULT_ROLE_ID', variable: 'VAULT_ROLE_ID'),
     ]) {
-      scm_branch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/builders/scm_branch?raw").trim()
       scm_url = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/builders/scm_url?raw").trim()
+      scm_branch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/builders/scm_branch?raw").trim()
       checkout_dir = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/builders/checkout_dir?raw").trim()
       checkout_info = checkout([$class: 'GitSCM',
         branches: [[name: scm_branch]],
