80d58f303a178b54a9fc41baa66586b437166886
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-27 10:47:12 +0300

    Work in progress

--- common/jobs/scm/compose-pod-build-pipeline.groovy
+++ common/jobs/scm/compose-pod-build-pipeline.groovy
@@ -1,32 +1,32 @@
 node {
   stage('checkout') {
-    scm_url = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/scm_url?raw").trim()
-    scm_branch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/scm_branch?raw").trim()
-    checkout_dir = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/checkout_dir?raw").trim()
-    if (checkout_dir == ".") {
-      checkout_info = checkout([$class: 'GitSCM',
-        branches: [[name: scm_branch]],
-        doGenerateSubmoduleConfigurations: false,
-        submoduleCfg: [],
-        userRemoteConfigs: [[url: scm_url]]]
-      )
-    } else {
-      checkout_info = checkout([$class: 'GitSCM',
-        branches: [[name: scm_branch]],
-        doGenerateSubmoduleConfigurations: false,
-        extensions:[
-          [$class: 'SparseCheckoutPaths', sparseCheckoutPaths:[[$class: 'SparseCheckoutPath', path: checkout_dir]]]
-        ],
-        submoduleCfg: [],
-        userRemoteConfigs: [[url: scm_url]]]
-      )
-    }
-  }
-  stage('build') {
     withCredentials([
         string(credentialsId: 'JENKINS_VAULT_TOKEN', variable: 'VAULT_TOKEN'),
         string(credentialsId: 'JENKINS_VAULT_ROLE_ID', variable: 'VAULT_ROLE_ID'),
     ]) {
+      // get project files
+      scm_url = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/scm_url?raw").trim()
+      scm_branch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/scm_branch?raw").trim()
+      checkout_dir = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/${POD_NAME}/checkout_dir?raw").trim()
+      if (checkout_dir == ".") {
+        checkout_info = checkout([$class: 'GitSCM',
+          branches: [[name: scm_branch]],
+          doGenerateSubmoduleConfigurations: false,
+          submoduleCfg: [],
+          userRemoteConfigs: [[url: scm_url]]]
+        )
+      } else {
+        checkout_info = checkout([$class: 'GitSCM',
+          branches: [[name: scm_branch]],
+          doGenerateSubmoduleConfigurations: false,
+          extensions:[
+            [$class: 'SparseCheckoutPaths', sparseCheckoutPaths:[[$class: 'SparseCheckoutPath', path: checkout_dir]]]
+          ],
+          submoduleCfg: [],
+          userRemoteConfigs: [[url: scm_url]]]
+        )
+      }
+      // get builders
       scm_url = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/builders/scm_url?raw").trim()
       scm_branch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/builders/scm_branch?raw").trim()
       checkout_dir = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/builders/checkout_dir?raw").trim()
@@ -40,8 +40,17 @@ node {
         submoduleCfg: [],
         userRemoteConfigs: [[url: scm_url]]]
       )
+    }
+  }
+  stage('build') {
+    withCredentials([
+        string(credentialsId: 'JENKINS_VAULT_TOKEN', variable: 'VAULT_TOKEN'),
+        string(credentialsId: 'JENKINS_VAULT_ROLE_ID', variable: 'VAULT_ROLE_ID'),
+    ]) {
+      checkout_dir = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/builders/checkout_dir?raw").trim()
       wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [
-        [password: 'thePassword', var: 'MY_PASSWORD']]]) {
+        [password: 'thePassword', var: 'MY_PASSWORD']
+      ]]) {
         sh(".builders/${checkout_dir}/compose-pod-build.sh")
       }
     }
