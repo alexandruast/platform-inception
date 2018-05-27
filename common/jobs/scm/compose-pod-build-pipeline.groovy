d27b8aeb50d667ac1891e4e09fbf9757d7de7fcc
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-27 09:27:07 +0300

    Work in progress

--- common/jobs/scm/compose-pod-build-pipeline.groovy
+++ common/jobs/scm/compose-pod-build-pipeline.groovy
@@ -13,9 +13,9 @@ node {
         string(credentialsId: 'JENKINS_VAULT_TOKEN', variable: 'VAULT_TOKEN'),
         string(credentialsId: 'JENKINS_VAULT_ROLE_ID', variable: 'VAULT_ROLE_ID'),
     ]) {
-      gitBranch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/shell/scm_branch?raw").trim()
-      gitURL = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/shell/scm_url?raw").trim()
-      gitDIR = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/shell/scm_dir?raw").trim()
+      gitBranch = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/builders/scm_branch?raw").trim()
+      gitURL = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/builders/scm_url?raw").trim()
+      gitDIR = sh(returnStdout: true, script: "curl -Ssf ${CONSUL_HTTP_ADDR}/v1/kv/platform-config/${PLATFORM_ENVIRONMENT}/builders/scm_dir?raw").trim()
       checkout_info = checkout([$class: 'GitSCM',
         branches: [[name: gitBranch]],
         doGenerateSubmoduleConfigurations: false,
