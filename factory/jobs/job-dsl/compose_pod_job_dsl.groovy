5b81439e45facd862b82b494c4c2faf2fef49356
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-28 11:00:21 +0300

    Work in progress

--- factory/jobs/job-dsl/compose_pod_job_dsl.groovy
+++ factory/jobs/job-dsl/compose_pod_job_dsl.groovy
@@ -1,10 +1,4 @@
 // ToDo: Retrieve this from Consul
-def environments = [
-  sandbox: [
-    services: ['sys-fluentd','sys-fabio','be-java-echo','be-go-demo'],
-    images: ['sys-py-yaml-to-consul']
-  ]
-]
 
 def jobSuffix='deploy'
 environments.each { environment ->
