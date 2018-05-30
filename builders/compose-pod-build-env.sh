2f025a8ac0a113eb55b90623ac40e205c7bb8c6b
Alexandru Ast <alexandru.ast@gmail.com>
2018-05-30 10:52:14 +0300

    Work in progress

--- builders/compose-pod-build-env.sh
+++ builders/compose-pod-build-env.sh
@@ -7,4 +7,4 @@ echo "[info] populating env file from consul..."
 ansible-playbook -i 127.0.0.1, \
   --connection=local \
   --module-path=${BUILDERS_DIR} \
-  ${BUILDERS_DIR}/build-env.yml >/dev/null
+  ${BUILDERS_DIR}/build-env.yml
