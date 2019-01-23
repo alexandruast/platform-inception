1e5a56345e094daa209818eca28d7c2ddb1e8591
Alexandru Ast <alexandru.ast@softvision.com>
2019-01-23 14:27:09 +0200

    Work in progress

--- builders/compose-service-build.sh
+++ builders/compose-service-build.sh
@@ -20,6 +20,7 @@ export REGISTRY_PASSWORD
 export BUILD_TAG
 
 for secret_key in $(echo "${VAULT_SECRETS:-}" | jq -re .[] | tr '\n' ' ' | sed -e 's/ $/ /'); do
+  echo $secret_key
   secret_value="$(curl -Ssf -X GET \
     -H "X-Vault-Token:${VAULT_TOKEN}" \
     "${VAULT_ADDR}/v1/secret/operations/${secret_key}" | jq -re .data.value)"
