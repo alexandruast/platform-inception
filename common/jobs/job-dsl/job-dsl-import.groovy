b70dc37460c9e93766eebfce14ba068666c27c65
Alexandru Ast <alexandru.ast@softvision.com>
2019-01-28 11:03:00 +0200

    Work in progress

---
+++ common/jobs/job-dsl/job-dsl-import.groovy
@@ -0,0 +1,44 @@
+import jenkins.model.Jenkins
+import hudson.plugins.git.GitSCM
+import hudson.plugins.git.BranchSpec
+import javaposse.jobdsl.plugin.*
+
+job_name = "system-${env.PLATFORM_SCOPE}-job-seed"
+job_description = "Dynamically created by jenkins-setup\nAny changes to this item will be overwritten without notice."
+git_repository = 'https://github.com/alexandruast/platform-inception'
+git_branch = '*/devel'
+set_targets = [
+  "${env.PLATFORM_SCOPE}/jobs/job-dsl/**/*.groovy",
+  "common/jobs/job-dsl/group_all/**/*.groovy",
+  "common/jobs/job-dsl/group_clusters/**/*.groovy"
+].join('\n')
+
+jenkins = Jenkins.instance
+
+// Delete existing job if exists
+jenkins.getItems().each {
+  if (it.name == job_name) {
+    it.delete()
+  }
+}
+
+job = jenkins.createProject(FreeStyleProject, job_name)
+
+job.setDescription(job_description)
+
+job.scm = new GitSCM(git_repository)
+job.scm.branches = [new BranchSpec(git_branch)]
+
+job.getBuildersList().clear()
+
+dslBuilder = new ExecuteDslScripts()
+dslBuilder.setTargets(set_targets)
+dslBuilder.setRemovedJobAction(RemovedJobAction.DISABLE)
+dslBuilder.setRemovedViewAction(RemovedViewAction.IGNORE)
+dslBuilder.setLookupStrategy(LookupStrategy.JENKINS_ROOT)
+
+job.createTransientActions()
+job.getBuildersList().add(dslBuilder)
+job.getPublishersList().add(dslBuilder)
+
+job.save()
